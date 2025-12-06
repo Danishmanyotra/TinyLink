10feef91e896b3952182b4b582ada8f89f26b525
Danishmanyotra <danishmanyotra@gmail.com>
2025-12-06 13:47:29 +0530

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,10 @@ import errorHandler from "./middlewares/errorHandler.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,DELETE",
+}));
 app.use(express.json());
 
 // healthcheck
